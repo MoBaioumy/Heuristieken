200470b29d2ebe579adacbbd83976b6aac84cb73
ThomasHoed <Thomas_hoed@live.nl>
2018-11-24 15:10:15 +0100

    distance.py comment

--- distance.py
+++ distance.py
@@ -1,3 +1,8 @@
 def distance(location_1, location_2):
+    """
+    Made for the SmartGrid assignment for Programmeertheorie
+    Take two location tuples as input
+    Returns the manhattan distance between these locations
+    """
     distance = abs(location_1[0] - location_2[0]) + abs(location_1[1] - location_2[1])
     return distance
